a13201bc630894ad3ebf82beb5eda552cecb7875
PratikMane@0112 <pratikmane610@gmail.com>
2024-08-19 02:37:21 +0530

    Final

--- src/spotify.js
+++ src/spotify.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const authEndpoint = process.env.REACT_APP_SPOTIFY_AUTH_ENDPOINT;
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+const redirectUri = process.env.REACT_APP_SPOTIFY_LOCAL_REDIRECT_URI || process.env.REACT_APP_SPOTIFY_VERCEL_REDIRECT_URI;
 const scopes = ["user-library-read", "playlist-read-private"];
 
 export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
@@ -10,7 +10,7 @@ export const loginEndpoint = `${authEndpoint}client_id=${clientId}&redirect_uri=
 )}&response_type=token&show_dialog=true`;
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_SPOTIFY_LOCAL_REDIRECT_URI || process.env.REACT_APP_SPOTIFY_VERCEL_REDIRECT_URI,
+  baseURL: process.env.REACT_APP_SPOTIFY_API_BASE_URL,
 });
 
 export const setClientToken = (token) => {
