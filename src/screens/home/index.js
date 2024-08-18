db3e2d19c4a3f3aad29d2699c429feb590c844b8
PratikMane@0112 <pratikmane610@gmail.com>
2024-08-19 02:02:25 +0530

    Update index.js

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Sidebar from "../../components/sidebar";
 import { setClientToken } from "../../spotify";
 import Login from "../auth/login";
@@ -12,7 +12,6 @@ import "./home.css";
 
 export default function Home() {
   const [token, setToken] = useState("");
-  const navigate = useNavigate(); // Add this line
 
   useEffect(() => {
     const token = window.localStorage.getItem("token");
@@ -23,77 +22,26 @@ export default function Home() {
       window.localStorage.setItem("token", _token);
       setToken(_token);
       setClientToken(_token);
-      navigate("/"); // Use navigate for redirection
     } else {
       setToken(token);
       setClientToken(token);
     }
-  }, [navigate]);
+  }, []);
 
   return !token ? (
     <Login />
   ) : (
-    <div className="main-body">
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Library />} />
-        {/* <Route path="/feed" element={<Feed />} />
-        <Route path="/trending" element={<Trending />} /> */}
-        <Route path="/player" element={<Player />} />
-        {/* <Route path="/favorites" element={<Favorites />} /> */}
-      </Routes>
-    </div>
+    <Router>
+      <div className="main-body">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Library />} />
+          {/* <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} /> */}
+          <Route path="/player" element={<Player />} />
+          {/* <Route path="/favorites" element={<Favorites />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-
-
-
-
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Sidebar from "../../components/sidebar";
-// import { setClientToken } from "../../spotify";
-// import Login from "../auth/login";
-// import Favorites from "../favorites";
-// import Feed from "../feed";
-// import Library from "../library";
-// import Player from "../player";
-// import Trending from "../trending";
-// import "./home.css";
-
-// export default function Home() {
-//   const [token, setToken] = useState("");
-
-//   useEffect(() => {
-//     const token = window.localStorage.getItem("token");
-//     const hash = window.location.hash;
-//     window.location.hash = "";
-//     if (!token && hash) {
-//       const _token = hash.split("&")[0].split("=")[1];
-//       window.localStorage.setItem("token", _token);
-//       setToken(_token);
-//       setClientToken(_token);
-//     } else {
-//       setToken(token);
-//       setClientToken(token);
-//     }
-//   }, []);
-
-//   return !token ? (
-//     <Login />
-//   ) : (
-//     <Router>
-//       <div className="main-body">
-//         <Sidebar />
-//         <Routes>
-//           <Route path="/" element={<Library />} />
-//           {/* <Route path="/feed" element={<Feed />} />
-//           <Route path="/trending" element={<Trending />} /> */}
-//           <Route path="/player" element={<Player />} />
-//           {/* <Route path="/favorites" element={<Favorites />} /> */}
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
